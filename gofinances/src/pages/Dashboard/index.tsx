4ba018623179f4eae8ce79a7527535b642fd12c6
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-09-12 10:39:26 -0300

    Proporções e ícones

--- gofinances/src/pages/Dashboard/index.tsx
+++ gofinances/src/pages/Dashboard/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import {Container, Title} from './styles'
+import React from "react";
 
-export function Dashboard(){
-  return(
-   <Container>
-    <Title>Dashboard</Title>
-   </Container> 
-  )
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/30416996?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Ricardo Girardi</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
 }
-
